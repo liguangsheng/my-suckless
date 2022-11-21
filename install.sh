f5f290b0aa182a6dc3e1c38cae01a180197fba50
Li Guangsheng <ligs.cn@gmail.com>
2022-11-21 10:49:52 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+(cd st     && rm -f config.h && sudo make install clean)
+(cd scroll && rm -f config.h && sudo make install clean)
+(cd dmenu  && rm -f config.h && sudo make install clean)
+(cd dwm    && rm -f config.h && sudo make install clean)
